833025eae84520b802b332d44c5bfca7d96b60ae
Negin <neginsaeidi@yahoo.com>
2020-05-12 17:36:22 +0000

    created a function that it returns True if the first letter of the string is the same as the last letter of the string

---
+++ string.py
@@ -0,0 +1,11 @@
+def first_and_last(message):
+  if message=="":
+    return True
+  if message[0]== message[len(message)-1]:
+    return True
+  else:
+    return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
+print(first_and_last(""))
