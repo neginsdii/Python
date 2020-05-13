25c2205d2e1c1d3a05a48d891b402f5885dc6212
Negin <neginsaeidi@yahoo.com>
2020-05-13 04:10:31 +0000

    added list exercises

---
+++ List/list.py
@@ -0,0 +1,12 @@
+def get_word(sentence, n):
+	if n > 0:
+		words = sentence.split()
+		if n <= len(words):
+			return(words[n-1])
+	return("")
+
+print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+print(get_word("This is a lesson about lists", -4)) # Nothing
+print(get_word("Now we are cooking!", 1)) # Should print: Now
+print(get_word("Now we are cooking!", 5)) # Nothing
+
