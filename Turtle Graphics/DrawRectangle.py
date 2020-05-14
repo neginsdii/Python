2d9759c7c9c7384853d3df8450af51e60c09f47f
Negin <neginsaeidi@yahoo.com>
2020-05-14 17:08:42 +0000

    added turtle methods

---
+++ Turtle Graphics/DrawRectangle.py
@@ -0,0 +1,12 @@
+import turtle
+
+window= turtle.Screen()
+window.bgcolor("lightyellow")
+new_trutle= turtle.Turtle()
+new_trutle.forward(100)
+new_trutle.left(90)
+new_trutle.forward(75)
+new_trutle.left(90)
+new_trutle.forward(100)
+new_trutle.left(90)
+new_trutle.forward(75)
